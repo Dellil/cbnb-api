173cad639fbd00732c9146ee335651edd90e4bc9
Dellil <aksaksdm@naver.com>
2020-04-18 05:36:12 +0900

    ah

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from .models import Room
+from .serializers import RoomSerializer
 
-# Create your views here.
+
+class ListRoomsView(ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+# FBV
+# @api_view(["GET"])
+# def list_rooms(request):
+#     rooms = Room.objects.all()
+#     serialized_rooms = RoomSerializer(rooms, many=True)
+
+#     return Response(data=serialized_rooms.data)
