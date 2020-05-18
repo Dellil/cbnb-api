c915e3af397e69931439f57bc02c1c7f09987fd1
Dellil <aksaksdm@naver.com>
2020-05-19 04:26:43 +0900

    viewset applied

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,6 @@ AUTH_USER_MODEL = "users.User"
 
 # Django Rest Framework
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': "rest_framework.pagination.PageNumberPagination",
-    "PAGI_SIZE": 10,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
